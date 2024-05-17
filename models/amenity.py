a3cedacdfcdba44b44ee19562e7fbc6dc9090d43
flowsar <brahim.sar151@gmail.com>
2024-05-17 12:56:25 +0100

    update

--- models/amenity.py
+++ models/amenity.py
@@ -5,5 +5,7 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     
+    name = ""
+
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
--- models/review.py
+++ models/review.py
@@ -6,5 +6,9 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     
+    place_id = ""
+    user_id = ""
+    text = ""
+
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
