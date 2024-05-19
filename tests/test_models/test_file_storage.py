ba91994e4cdd4405b6984f6b199d6848aaef8196
flowsar <brahim.sar151@gmail.com>
2024-05-19 16:14:19 +0100

    up

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -1,2 +1,14 @@
-#!/usr/bin/python3 
- 
+#!/usr/bin/python3
+"""this test module fo testing on storage file"""
+import unittest
+import json
+from models.place import Place
+
+
+class TestFileStorage(unittest.TestCase):
+    """this test class fo testing on storage file"""
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
